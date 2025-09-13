65b98827905a0872a9636d21d13344c40862727b
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 19:43:25 +0800

    1st commit of in-lukkho

--- Filmo.java
+++ Filmo.java
@@ -1,5 +1,5 @@
 public class Filmo {
     public static void main(String[] args) {
-        System.out.println("we are on branch main");
+        System.out.println("bihar ka naam badnaam mat kiya karo!");
     }
 }
