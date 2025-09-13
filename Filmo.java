964e3352d470a21b647883cccf03f525fd4a3008
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 19:52:21 +0800

    ek flop film ke saath

--- Filmo.java
+++ Filmo.java
@@ -2,5 +2,6 @@ public class Filmo {
     public static void main(String[] args) {
         System.out.println("bihar ka naam badnaam mat kiya karo!- the kashmir files - >");
         System.out.println("number 1 country");
+        System.out.println("bahut badi disaster!");
     }
 }
