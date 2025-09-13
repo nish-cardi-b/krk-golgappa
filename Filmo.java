f3f77c6a96c0290392a956802f6b4887b2388b59
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 20:09:25 +0800

    saif

--- Filmo.java
+++ Filmo.java
@@ -1,7 +1,5 @@
 public class Filmo {
     public static void main(String[] args) {
-        System.out.println("we are on branch main");
-        System.out.println("on another branch");
-        //gaana-denge
+        //wamiq - gabbi
     }
 }
