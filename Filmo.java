ccaae7d3f8f2f88ce793160752abcf8a4ed271ab
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 19:57:27 +0800

    in flop film final

--- Filmo.java
+++ Filmo.java
@@ -1,7 +1,5 @@
 public class Filmo {
     public static void main(String[] args) {
-        System.out.println("bihar ka naam badnaam mat kiya karo!- the kashmir files - >");
-        System.out.println("number 1 country");
-        System.out.println("bahut badi disaster!");
+        System.out.println("we are on branch main");
     }
 }
